14dd5372bfb6f455e929f6aa99c40a3072b285a8
supermikea <mike12wiegman@gmail.com>
2022-11-06 15:53:18 +0100

    initial commit

---
+++ src/client.py
@@ -0,0 +1,19 @@
+import socket
+
+class client:
+	def __init__(self, name, port, ip):
+		self.name = name
+		self.port = port
+		self.ip = ip
+	
+	def connect(self):
+		print("connect function invoked!")
+		s = socket.socket()
+
+		s.connect((self.ip, self.port))
+
+		while True:
+			print(s.recv(1024).decode())
+
+user = client('name', 1024, '127.0.0.1')
+user.connect()
